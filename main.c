ef13495f0c24a8b687f23eb4baf73409f763072b
MariaIzamara <54847079+MariaIzamara@users.noreply.github.com>
2019-10-13 10:57:09 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,111 @@
+#include "libraries.h"
+#include "variables.h"
+
+GLuint carregaTextura(const char* arquivo) {
+    GLuint idTextura = SOIL_load_OGL_texture(
+                           arquivo,
+                           SOIL_LOAD_AUTO,
+                           SOIL_CREATE_NEW_ID,
+                           SOIL_FLAG_INVERT_Y
+                       );
+
+    if (idTextura == 0) {
+        printf("Erro do SOIL: '%s'\n", SOIL_last_result());
+    }
+
+    return idTextura;
+}
+
+void desenhaChao(GLuint id, dimpos objeto){
+    glBindTexture(GL_TEXTURE_2D, id);
+    glPushMatrix();
+        glTranslatef(objeto.x, objeto.y, 0);
+        glBegin(GL_TRIANGLE_FAN);
+            glTexCoord2f(0, 0); glVertex3f(-objeto.l/2, -objeto.a/2, 0);
+            glTexCoord2f(1, 0); glVertex3f(objeto.l/2, -objeto.a/2, 0);
+            glTexCoord2f(1, 1); glVertex3f(objeto.l/2, objeto.a/2, 0);
+            glTexCoord2f(0, 1); glVertex3f(-objeto.l/2, objeto.a/2, 0);
+        glEnd();
+    glPopMatrix();   
+}
+
+
+void inicializa() {
+    glClearColor(1, 1, 1, 1);
+    srand(time(0));
+    // habilita mesclagem de cores, para termos suporte a texturas com transparência
+    glEnable(GL_BLEND );
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    idTexturaChao = carregaTextura("images/grass.jpg");
+
+    mundo.l = 1000;
+    mundo.a = 615;
+}
+
+void redimensiona(int width, int height) {
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, mundo.l, 0, mundo.a, -1, 1);
+
+    float razaoAspectoJanela = ((float)width)/height;
+    float razaoAspectoMundo = ((float) mundo.l)/ mundo.a;
+    // se a janela está menos larga do que o mundo (16:9)...
+    if (razaoAspectoJanela < razaoAspectoMundo) {
+        // vamos colocar barras verticais (acima e abaixo)
+        float hViewport = width / razaoAspectoMundo;
+        float yViewport = (height - hViewport)/2;
+        glViewport(0, yViewport, width, hViewport);
+    }
+    // se a janela está mais larga (achatada) do que o mundo (16:9)...
+    else if (razaoAspectoJanela > razaoAspectoMundo) {
+        // vamos colocar barras horizontais (esquerda e direita)
+        float wViewport = ((float)height) * razaoAspectoMundo;
+        float xViewport = (width - wViewport)/2;
+        glViewport(xViewport, 0, wViewport, height);
+    } else {
+        glViewport(0, 0, width, height);
+    }
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+void teclado(unsigned char key, int x, int y) {
+    switch (key) {
+        case 27: // tecla 'esc'
+            exit(0);
+            break;
+    }
+}
+
+void atualizaCena(int periodo) {
+
+    // pede ao GLUT para redesenhar a tela, assim que possível
+    glutPostRedisplay();
+
+    // se registra novamente, para que fique sempre sendo chamada
+    glutTimerFunc(periodo, atualizaCena, periodo);
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(1000, 615);
+    glutInitWindowPosition(100, 100);
+
+    glutCreateWindow("Haunted Park");
+    inicializa();
+
+    glutDisplayFunc(desenhaChao);    
+    glutReshapeFunc(redimensiona);
+
+
+    glutKeyboardFunc(teclado);
+
+    glutTimerFunc(0, atualizaCena, 33);
+
+    glutMainLoop();
+
+    return 0;
+}
---
+++ variables.h
@@ -0,0 +1,8 @@
+typedef struct {
+    GLfloat l, a, x, y; // l = largura, a = altura
+} dimpos; // tudo que tem dimensões e/ou posição.
+
+dimpos mundo;
+
+GLuint idTexturaChao; // imagem do chão
+
